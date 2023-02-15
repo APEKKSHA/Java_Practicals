243ff293fa1cc64ae37d9a7dbf96aaf289aade74
Apeksha <98733655+APEKKSHA@users.noreply.github.com>
2023-02-15 13:52:46 +0530

    Add files via upload

---
+++ SQL_Queries_Module-3/Emply_log.sql
@@ -0,0 +1,7 @@
+USE Practical_Data;
+CREATE TABLE Emply_log(
+eid int(5) NOT NULL,
+log_date DATE,
+new_salary int(10),
+action varchar(20)
+);
---
+++ SQL_Queries_Module-3/Student_data.sql
@@ -0,0 +1,8 @@
+USE Practical_Data;
+CREATE TABLE Student_data(
+rno int(2) NOT NULL,
+sname varchar(14),
+city varchar(20),
+state varchar(20),
+PRIMARY KEY(rno)
+);
