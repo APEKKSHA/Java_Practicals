1db199c2be9bbceed86d141ac30c014a7450e6e4
Apeksha <98733655+APEKKSHA@users.noreply.github.com>
2023-03-31 22:43:57 +0530

    Add files via upload

---
+++ Module_5/Phone.java
@@ -0,0 +1,21 @@
+package com.sp2;
+
+public class Phone 
+{
+	int pno;
+	// its an setter getter logic
+
+	public int getPno() {
+		return pno;
+	}
+
+	public void setPno(int pno) {
+		this.pno = pno;
+	}
+	
+//	to create seperate method which is used in person class
+	public void call()
+	{
+		System.out.println("phone no is.."+pno);
+	}
+}
